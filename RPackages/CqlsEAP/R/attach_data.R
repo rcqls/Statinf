57b9cac4c214b3989b66d80a249cc38bcab46f81
R cqls <rdrouilh@gmail.com>
2014-07-07 07:33:05 +0200

    path.data added

--- RPackages/CqlsEAP/R/attach_data.R
+++ RPackages/CqlsEAP/R/attach_data.R
@@ -1,9 +1,13 @@
+path.data <- function(...,package="CqlsEAP") {
+	system.file("data",...,package=package)
+}
+
 attach.data <- function(...,package="CqlsEAP") {
-	attach(system.file("data",...,package=package))
+	attach(path.data(...,package=package))
 }
 
 detach.data <- function(...,package="CqlsEAP") {
-	filename <- system.file("data",...,package=package)
+	filename <- path.data(...,package=package)
 	pos <- which(search() %in% paste("file",filename,sep=":"))
 	# eval since detach does not work undirectly with integer and character vector
 	if(length(pos)>0) eval(parse(text=paste("detach(",pos[1],")",sep="")))
